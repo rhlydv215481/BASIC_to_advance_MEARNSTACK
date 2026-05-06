f70c60685913f1eeb926e6c034dae8ca4284f43e
Rahul Yadav <rhlydv216581@outlook.com>
2026-05-06 14:28:54 +0530

    understanding basic of dom like slecting id h1 and chnge innertext,html bg colour and counter number by clicking

---
+++ dom/day41/first.js
@@ -0,0 +1,34 @@
+let H1 = document.querySelector('h1');
+let H2 = document.querySelector('h2');
+console.dir(H1);
+console.dir(H2);
+H1.innerHTML = "manupulating ";
+// ye bus content ko change karega
+H2.outerHTML = "<h3> hello yadavs</h3>"
+// ye  selector ko remove kar deta hai issa leya elmet ko remove kar deya
+let but = document.querySelector('button');
+but.addEventListener('click',function(){
+    console.log('tunna click kiya hai ');
+    but.innerText=" you press the button";
+    but.style.backgroundColor="yellow";
+    but.style.padding="8px";
+    H1.innerHTML=" i am Rahul";
+    H1.style.color="red";
+    H1.style.fontSize='60px';
+})
+
+var h4 = document.querySelector('h4');
+var inc = document.querySelector('#inc');
+a=0;
+
+ inc.addEventListener('click',function(){
+    a++;
+    h4.innerHTML = a;
+});
+var dec = document.querySelector("#dec");
+ dec.addEventListener('click',function(){
+    // console.log('you click')ṭ
+    a--;
+    h4.innerText = a;
+})
+
