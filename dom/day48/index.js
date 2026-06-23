dc22aa8e5057d6b7709075f9906f590d83df5b97
Rahul Yadav <rhlydv216581@outlook.com>
2026-06-23 16:29:19 +0530

    cursor move horahai tho arror bhi move horahai

---
+++ dom/day48/index.js
@@ -0,0 +1,30 @@
+let box = document.querySelector('#box');
+let img = document.querySelector('img');
+// box.addEventListener('click',()=>{
+    //     console.log('you clicked the  the box')
+    // });
+    // box.addEventListener('dblclick',()=>{
+        //     console.log('you double clicked ');
+        
+        // })
+        // box.addEventListener('mouseenter',()=>{
+            //     console.log("mouse entre")
+            // })
+            // box.addEventListener('mouseleave',()=>{
+                //     console.log("left ..")
+                // })
+                // main.addEventListener('mousemove',(detail)=>{
+                    //  img.style.left = detail.x + 'px'
+                    //  img.style.top = detail.y + 'px'
+                    //  console.log( detail.y)
+                    // })
+                    // box.addEventListener('wheel',(detsil)=>{
+                        //     console.log(detsil);
+                        
+                        // })
+                        // l = document.querySelector('h1');
+                        let body = document.querySelector('body')
+                        // let main = document.querySelector('main');
+window.addEventListener('keydown',(detail)=>{
+console.log(detail.key)
+})
