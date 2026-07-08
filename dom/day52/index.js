4573e4bd0cc55bf016bb571d79d27a8d94036401
Rahul Yadav <rhlydv216581@outlook.com>
2026-07-09 04:59:08 +0530

    i try with muself and watch youtube and with the help of chatgtp i build spootlight

--- dom/day52/index.js
+++ dom/day52/index.js
@@ -1,6 +1,35 @@
-let section = document.querySelector('section');
+let tops = document.querySelector('.top');
+let h1 = document.querySelectorAll('h1')
+let h2 = document.querySelector('h2')
+let section = document.querySelector('section')
+
 section.addEventListener('mousemove',(det)=>{
     document.body.style.setProperty("--x",det.clientX+'px')
     
     document.body.style.setProperty("--y",det.clientY+'px')
+})
+console.log('hello')
+let abc = 'ABCDEFGHIJKLMNOPQURSTYXZabcdefghijklmonpquerstyx';
+h1.forEach((det) => {
+     let text = det.innerText
+det.addEventListener('mouseenter',()=>{
+    // console.log(text.split(''));
+    // console.log('hello')
+    let repeated = setInterval(() => {
+        
+        
+        let str = text.split('').map(()=>{
+            return  abc[Math.floor(Math.random() * abc.length)]
+        }).join('')
+        det.innerText = str;
+    }, 90);
+    setTimeout(() => {
+        clearInterval(repeated)
+        det.style.color = 'rgb(74, 207, 103)'
+        // det.style.opacity='0.4'
+       det.innerText= text
+    }, 2000);
+    // det.style.opacity='0'
+    //   console.log(str)
+    });
 })
