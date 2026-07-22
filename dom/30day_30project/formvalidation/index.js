278c1dbc14f8bade087da1694cb86b3fe2b3fc73
Rahul Yadav <rhlydv216581@outlook.com>
2026-07-22 17:41:55 +0530

    form validation eith my logic

--- dom/30day_30project/formvalidation/index.js
+++ dom/30day_30project/formvalidation/index.js
@@ -1,53 +1,65 @@
-let form = document.querySelector("form");
-let p = document.querySelector("p");
-let password = document.querySelector("#password")
-let gmail = document.querySelector('#gmail')
-let btn = document.querySelector('button');
+
+
+let input = document.querySelectorAll("input");
 let small = document.querySelectorAll("small")
-btn.addEventListener("click",(event)=>{
-    event.preventDefault();
-   const passwordRegex = /^.{8,}$/;
-;
+let btn = document.querySelector("button");
 
-    const regex = /^([\w]*[\w.]*(?!\.)@gmail\.com)$/;
-let ans = regex.test(gmail.value);
-
-    btn.addEventListener("click", (event) => {
-    event.preventDefault();
-
-const inputs = document.querySelectorAll("input");
-
-inputs.forEach((input) => {
-    const small = input.nextElementSibling;
-
-    if (input.value.trim() === "") {
-        small.innerText = `${input.id} is empty`;
-        return;
-    } 
-});
-// Step 2: Gmail check
-    if (!regex.test(gmail.value)) {
-        console.log("Invalid Gmail");
-        return;
+console.log(input)
+console.log("hello")
+// console.log(usernameRegex);
+console.log(small)
+btn.addEventListener("click", (event) => {
+    let isValid = true;
+    event.preventDefault(); 
+    input.forEach((ele,index) => {
+        console.log(ele.id);
+// console.log(ele.value);
+console.log(ele.value)
+        // console.log(ele.id)
+        if(ele.value.trim() === ""){
+            // console.log(small)
+                 small[index].innerText = `plz entre value ${ele.id}`;
+                 small[index].style.color = ' red'
+               isValid = false;
+                //  form.reset();
+        }
+        else{
+            const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+// console.log(usernameRegex);
+if( ele.id === 'username') {
+  
+    // console.log("noy")
+    // console.log(ele.value) // ye value leya 
+  if(!usernameRegex.test(ele.value)){
+    isValid = false;
+       small[index].innerText = `plz entre corrected username` // small[index] iss pata chal rahai ki 0 elemt walla mai karna hai 
+    small[index].style.color = ' red'
     }
-
-    // Step 3: Password check
-    else if  (!passwordRegex.test(password.value)) {
-        console.log("Invalid Password");
-        return;
-    }
-    else{
-        // Step 4: Sab sahi hai
-     p.innerText = " Form Submitted Successfully ";    }
-
-    form.reset()
 }
-);
+else if (ele.id  === 'gmail' ){ // id gmail hai 
+    if(!gmailRegex.test(ele.value)) {//gmail ka value patten same nhi hai tho {
+        isValid = false;
+        small[index].style.color = ' red'
+    small[index].innerText ='plz entre valied gmail and it should be and gmail.com'    
+} 
+}
+else if (ele.id === 'password'){
+    if(!passwordRegex.test(ele.value)){
+        isValid = false;
+  small[index].style.color = ' red'
+        small[index].innerText ='plz entre valied password 1 small letter  1 capital letter  1 number  1 special character  8 characters'
+    }
+}
 
-})
-
-
- 
- 
-// console.log(ans)
-    // 
+        }
+        
+     
+     });
+    if (isValid === true) {
+        small[4].innerText = `for submitted succesfully`
+        small[4].style.color = ' green'
+    }
+    // console.log("hello");
+});
